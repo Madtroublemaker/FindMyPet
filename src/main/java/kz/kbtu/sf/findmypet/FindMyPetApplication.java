19d139491d372d13c94087464816beb9a0805e24
madinaAbd <abdulgafarova.madina@mail.ru>
2025-03-11 16:18:57 +0500

    o;ok

---
+++ src/main/java/kz/kbtu/sf/findmypet/FindMyPetApplication.java
@@ -0,0 +1,13 @@
+package kz.kbtu.sf.findmypet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindMyPetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/kbtu/sf/findmypet/FindMyPetApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.kbtu.sf.findmypet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMyPetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
